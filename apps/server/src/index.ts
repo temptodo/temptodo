d05c5e630a7d146d2bbde67d0ea7baf785ca91b9
Dmytro Leonov <leonovdmitriy02@gmail.com>
2026-09-05 22:18:08 +0300

    chore: http api

--- apps/server/src/index.ts
+++ apps/server/src/index.ts
@@ -1,3 +1,30 @@
-import { Effect as E, Console } from 'effect';
+import { NodeHttpServer, NodeRuntime } from '@effect/platform-node';
+import { Effect, Layer, Schema } from 'effect';
+import { HttpRouter } from 'effect/unstable/http';
+import {
+  HttpApi,
+  HttpApiBuilder,
+  HttpApiEndpoint,
+  HttpApiGroup,
+} from 'effect/unstable/httpapi';
+import { createServer } from 'node:http';
 
-E.runSync(Console.log('Hello, World!'));
+const Api = HttpApi.make('MyApi').add(
+  HttpApiGroup.make('Greetings').add(
+    HttpApiEndpoint.get('hello', '/', {
+      success: Schema.String,
+    }),
+  ),
+);
+
+const GroupLayer = HttpApiBuilder.group(Api, 'Greetings', (handlers) =>
+  handlers.handle('hello', () => Effect.succeed('Hello, World!')),
+);
+
+const ApiLayer = HttpApiBuilder.layer(Api).pipe(
+  Layer.provide(GroupLayer),
+  HttpRouter.serve,
+  Layer.provide(NodeHttpServer.layer(createServer, { port: 3000 })),
+);
+
+Layer.launch(ApiLayer).pipe(NodeRuntime.runMain);
