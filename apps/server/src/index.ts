1d82ba52be4966a3b352cfb58b24fc123952f30f
Dmytro Leonov <leonovdmitriy02@gmail.com>
2026-09-06 19:31:28 +0300

    chore: POC config

--- apps/server/src/index.ts
+++ apps/server/src/index.ts
@@ -2,17 +2,47 @@ import { createServer } from 'node:http';
 
 import { NodeHttpServer, NodeRuntime } from '@effect/platform-node';
 import { Api } from '@repo/api-definition';
-import { Effect, Layer } from 'effect';
+import {
+  Config,
+  Effect,
+  Context,
+  Layer,
+  Console,
+  Redacted,
+  Schema,
+} from 'effect';
 import { HttpRouter } from 'effect/unstable/http';
 import { HttpApiBuilder } from 'effect/unstable/httpapi';
 
+const DatabaseConfig = Config.all({
+  url: Config.schema(Schema.Redacted(Schema.URL), 'URL'),
+}).pipe(Config.nested('DATABASE'));
+
+const appConfig = Config.all({
+  db: DatabaseConfig,
+});
+
+export class AppConfig extends Context.Service<
+  AppConfig,
+  Config.Success<typeof appConfig>
+>()('AppConfig') {}
+
+const AppConfigLive = Layer.effect(AppConfig, appConfig);
+
 const GroupLayer = HttpApiBuilder.group(Api, 'Greetings', (handlers) =>
-  handlers.handle('hello', () => Effect.succeed('Hello, World!')),
+  handlers.handle('hello', () =>
+    Effect.gen(function* () {
+      const config = yield* AppConfig;
+      yield* Console.log(Redacted.value(config.db.url));
+      return '';
+    }),
+  ),
 );
 
 const ApiLayer = HttpApiBuilder.layer(Api).pipe(
   Layer.provide(GroupLayer),
   HttpRouter.serve,
+  Layer.provide(AppConfigLive),
   Layer.provide(NodeHttpServer.layer(createServer, { port: 8000 })),
 );
 
