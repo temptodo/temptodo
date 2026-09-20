a03a7392aa771eaee27f8fca59d822fb1c354162
sl-chern <slavikchernogor@gmail.com>
2026-09-20 16:20:59 +0300

    fix(client): move api call from layout to server component

--- apps/client/src/app/page.tsx
+++ apps/client/src/app/page.tsx
@@ -1,3 +1,19 @@
-export default function Home(): React.ReactNode {
-  return <div>Hello, World!</div>;
+import { Api } from '@repo/api-definition';
+import { Effect } from 'effect';
+import { FetchHttpClient } from 'effect/unstable/http';
+import { HttpApiClient } from 'effect/unstable/httpapi';
+
+const test = Effect.gen(function* test() {
+  const client = yield* HttpApiClient.make(Api, {
+    baseUrl: 'http://127.0.0.1:8000',
+  });
+  return yield* client.Greetings.hello();
+}).pipe(Effect.provide(FetchHttpClient.layer));
+
+export const dynamic = 'force-dynamic';
+
+export default async function Home(): Promise<React.ReactNode> {
+  const res = await Effect.runPromise(test);
+
+  return <div>Hello, World! | {res}</div>;
 }
